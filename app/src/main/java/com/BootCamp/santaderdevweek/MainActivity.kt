51f5fa6ca877753fa761866321a7bbed2a6509d3
anderson <Prey.anderson2@gmail.com>
2021-06-07 15:44:25 -0300

    Teste da aplicação não funciona.

--- app/src/main/java/com/BootCamp/santaderdevweek/MainActivity.kt
+++ app/src/main/java/com/BootCamp/santaderdevweek/MainActivity.kt
@@ -8,12 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+     //   setSupportActionBar(findViewById(R.id.toolbar))
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflate = menuInflater
-        inflate.inflate(R.menu.main_menu, menu)
-        return true
-    }
+
 }
